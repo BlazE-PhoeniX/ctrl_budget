a63e70d747e1ed4a990bcb0f8f23b19799743e3f
Hariprasath <53620434+BlazE-PhoeniX@users.noreply.github.com>
2020-07-19 17:52:49 +0530

    Source file uploads

---
+++ ctrlbudget/includes/footer.php
@@ -0,0 +1,11 @@
+
+<!--footer content-->
+<footer class="text-center py-3 text-white">
+  <div>
+	<p>
+	  Copyright&nbsp;&copy;&nbsp;Ctrl&nbsp;Budget.
+	  All&nbsp;Rights&nbsp;Reserved
+	  <span class="d-none d-sm-inline">|</span>​ Contact:&nbsp;+91&nbsp;90000&nbsp;00000
+	</p>
+  </div>
+</footer>
