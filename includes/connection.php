067c58caa784d09d3851f74d1f03b510f86609fc
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-09 00:30:16 +0530

    configured database

--- includes/connection.php
+++ includes/connection.php
@@ -1,10 +1,10 @@
 <?php
 
 //host, username, password, and database name
-$host= "";
-$username= "";
-$password= "";
-$dbname= "";
+$host= "z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username= "wbe4wvj4jqznev8b";
+$password= "xrgh65v0ro991637";
+$dbname= "xqnwdg697eygsysv";
 
 //make an connection with the data base
 $con = mysqli_connect( $host, $username, $password, $dbname)
