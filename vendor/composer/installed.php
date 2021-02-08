2f3fcc4537d0fa5de39892d5596c8b6f6b7b6a88
BlazE-PhoeniX <hariprasathh01@gmail.com>
2021-02-09 00:16:15 +0530

    added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0e27698a130d1a160acedade50a85b771712f27f',
+    'name' => 'control/budget',
+  ),
+  'versions' => 
+  array (
+    'control/budget' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0e27698a130d1a160acedade50a85b771712f27f',
+    ),
+  ),
+);
